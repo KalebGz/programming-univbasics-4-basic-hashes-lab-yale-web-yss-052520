6e3c373bad3287dd4e7d8399f2e682d4e122e028
Kaleb Gezahegn <kaleb.gezahegn@yale.edu>
2020-05-19 06:36:51 +0000

    Automatically backed up by Learn

--- lib/intro_to_ruby_hashes_lab.rb
+++ lib/intro_to_ruby_hashes_lab.rb
@@ -1,13 +1,19 @@
 def new_hash
-  # return an empty hash
+  hash = {}
 end
 
 def my_hash
-  # return a valid hash with any key/value pair of your choice
+  animals = {
+    name: "ralph"
+    type: "dog"
+  }
 end
 
 def pioneer
-  # return a hash with a key of :name and a corresponding value of 'Grace Hopper'
+  animals = {
+    name: "ralph"
+    type: "dog"
+  }
 end
 
 
